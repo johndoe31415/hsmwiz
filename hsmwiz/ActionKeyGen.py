ac367a2b2fe140fad2660ca48b61b9b5c8310c0f
Johannes Bauer <joe@johannes-bauer.com>
2020-08-18 11:06:07 +0200

    Change copyright notice
    Reflect the change in name in all copyright notices.

--- hsmwiz/ActionKeyGen.py
+++ hsmwiz/ActionKeyGen.py
@@ -1,14 +1,14 @@
-#	nitrotool - Frontend for NitroKey USB HSM
+#	hsmwiz - Frontend for NitroKey USB HSM
 #	Copyright (C) 2018-2020 Johannes Bauer
 #
-#	This file is part of nitrotool.
+#	This file is part of hsmwiz.
 #
-#	nitrotool is free software; you can redistribute it and/or modify
+#	hsmwiz is free software; you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
 #	the Free Software Foundation; this program is ONLY licensed under
 #	version 3 of the License, later versions are explicitly excluded.
 #
-#	nitrotool is distributed in the hope that it will be useful,
+#	hsmwiz is distributed in the hope that it will be useful,
 #	but WITHOUT ANY WARRANTY; without even the implied warranty of
 #	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #	GNU General Public License for more details.
